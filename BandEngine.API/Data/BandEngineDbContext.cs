b77b0ac57886601aacb2873bef9e7d859e9d33e9
Adam Neujahr <adamneujahr@gmail.com>
2020-09-17 11:21:18 -0500

    Remove BandEngineDatabaseSettings

--- BandEngine.API/Data/BandEngineDbContext.cs
+++ BandEngine.API/Data/BandEngineDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BandEngine.API.Models;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,13 @@ namespace BandEngine.API.Data
 {
     public class BandEngineDbContext
     {
+        private readonly IMongoDatabase _db;
+
+        public BandEngineDbContext(IMongoClient client, string dbName)
+        {
+            _db = client.GetDatabase(dbName);
+        }
+
+        public IMongoCollection<User> Users => _db.GetCollection<User>("users");
     }
 }
