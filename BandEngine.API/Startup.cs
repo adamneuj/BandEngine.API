97134811e886f83aff139d3b14df6e4a50538cca
Adam Neujahr <adamneujahr@gmail.com>
2020-09-17 11:16:06 -0500

    Add custom DbContext

--- BandEngine.API/Startup.cs
+++ BandEngine.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BandEngine.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace BandEngine.API
 {
@@ -25,6 +27,12 @@ namespace BandEngine.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<BandEngineDatabaseSettings>(
+                Configuration.GetSection(nameof(BandEngineDatabaseSettings)));
+
+            services.AddSingleton<IBandEngineDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<BandEngineDatabaseSettings>>().Value);
+
             services.AddControllers();
         }
 
