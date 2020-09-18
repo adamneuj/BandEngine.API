aec96c3d887767eda5a9cad493d69b3022c8dd56
Adam Neujahr <adamneujahr@gmail.com>
2020-09-18 10:46:33 -0500

    Remove references for MongoDb

--- BandEngine.API/Services/UserService.cs
+++ BandEngine.API/Services/UserService.cs
@@ -1,6 +1,5 @@
 ﻿using BandEngine.API.Data;
 using BandEngine.API.Models;
-using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +9,5 @@ namespace BandEngine.API.Services
 {
     public class UserService
     {
-        private readonly BandEngineDbContext _db;
-
-        public UserService(BandEngineDbContext db)
-        {
-            _db = db;
-        }
-
-        public async Task<User> GetUserAsync(string id)
-        {
-            return await _db.Users.Find(u => u.Id == id).FirstAsync();
-        }
-
-        public async Task<bool> RegisterUserAsync(User user)
-        {
-            try
-            {
-                await _db.Users.InsertOneAsync(user);
-                return true;
-            }
-            catch(AggregateException e)
-            {
-                return false;
-            }
-
-        }
     }
 }
--- BandEngine.API/Startup.cs
+++ BandEngine.API/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BandEngine.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,8 +11,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson.Serialization.Conventions;
-using MongoDB.Driver;
 
 namespace BandEngine.API
 {
@@ -29,9 +26,6 @@ namespace BandEngine.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            ConventionRegistry.Register("Camel Case", new ConventionPack { new CamelCaseElementNameConvention() }, _ => true);
-            services.AddSingleton<IMongoClient>(s => new MongoClient(Configuration.GetConnectionString("MongoDb")));
-            services.AddScoped(s => new BandEngineDbContext(s.GetRequiredService<IMongoClient>(), Configuration["DbName"]));
 
             services.AddControllers();
         }
