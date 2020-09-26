b7846b0bafde0ed6ca5a5fa4772e4c85a68598cd
Adam Neujahr <adamneujahr@gmail.com>
2020-09-26 17:48:44 -0500

    Add band model and ientity interface

---
+++ BandEngine.API/Models/Band.cs
@@ -0,0 +1,18 @@
+﻿using BandEngine.API.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandEngine.API.Models
+{
+    public class Band : IEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid DataOwner { get; set; }
+
+    }
+}
